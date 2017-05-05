50c4485ebe79efdfb61ff771c1e12a3ba23ac5ba
szopqa <szopqa96@gmail.com>
2017-05-05 17:03:35 +0200

    Added slider menu animation

--- Pomodoro/View/Pages/MainApplicationPages/ApplicationStartPage.xaml.cs
+++ Pomodoro/View/Pages/MainApplicationPages/ApplicationStartPage.xaml.cs
@@ -2,25 +2,60 @@
 using System.Windows.Controls;
 using Pomodoro.Model;
 using Pomodoro.ViewModel;
+using System.Windows.Media.Animation;
 
 namespace Pomodoro.View.Pages.MainApplicationPages {
 	/// <summary>
 	/// Interaction logic for ApplicationStartPage.xaml
-	/// </summary>
+	/// </summary>	
 	public partial class ApplicationStartPage : Page {
 
 		private ApplicationStartPageViewModel viewModel = new ApplicationStartPageViewModel();
+		private bool isSliderMenuVisible;
 
-		public ApplicationStartPage ( User loggedUSer ) {
+
+		public ApplicationStartPage(User loggedUSer) {
 			InitializeComponent();
+
+			//Menu is being shown onload of page
+			this.isSliderMenuVisible = true;
 
 			//Passing loggedUser and page object 
 			viewModel.LoggedUser = loggedUSer;
 			viewModel.ApplicationStartPage = this;
 
-			//TODO : Wywalić to z konstruktora!
+			//Loads user data on start page
+			loadLoggedUserData();
+		}
+
+
+
+		/// <summary>
+		/// Loads data about logged user onload of start page
+		/// </summary>
+		private void loadLoggedUserData() {
+			//TODO : Use onload method instead of invoking this in constructor
 			viewModel.ShowLoggedUserInfo();
 		}
-		
+
+
+		private void MenuButtonClick ( object sender, RoutedEventArgs e ) {
+
+			Storyboard showMenu = this.FindResource("showDockPanel") as Storyboard;
+			Storyboard hideMenu = this.FindResource("showDockPanel") as Storyboard;
+
+			if ( ! isSliderMenuVisible ) {
+				showMenu = this.FindResource("showDockPanel") as Storyboard;
+				showMenu.Begin();
+				isSliderMenuVisible = true;
+			}
+			else {
+				hideMenu = this.FindResource("hideDockPanel") as Storyboard;
+				hideMenu.Begin();
+				isSliderMenuVisible = false;
+			}
+
+
+		}
 	}
 }
