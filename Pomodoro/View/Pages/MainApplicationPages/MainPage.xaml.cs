2b914d47123c825bd67afe60a7147214d1951e3f
szopqa <szopqa96@gmail.com>
2017-05-09 20:52:34 +0200

    Added UserInfoPage as main page

---
+++ Pomodoro/View/Pages/MainApplicationPages/MainPage.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Pomodoro.Model;
+using Pomodoro.ViewModel;
+using System.Windows.Media.Animation;
+using Pomodoro.ViewModel.MainApplicationPagesViewModel;
+
+namespace Pomodoro.View.Pages.MainApplicationPages {
+	
+	/// <summary>
+	/// Application's main page. Holds logged user info, passing it to another pages
+	/// </summary>	
+	public partial class MainPage : Page {
+
+	#region Properties
+		
+		/*Page objects*/
+		private UserInfoPage userInfoPage = new UserInfoPage();
+		private PomodoroTimerPage pomodoroTimerPage = new PomodoroTimerPage();
+		
+		/*View Model pages*/
+		private MainPageViewModel _mainViewModel;
+		private UserInfoPageViewModel _userInfoViewModel;
+		private PomodoroTimerPageViewModel _timerViewModel;
+		
+		private bool isSliderMenuVisible;
+
+	#endregion
+
+		public MainPage(User loggedUser) {
+
+			InitializeComponent();
+
+			//Passing loggedUser and pages to their viewmodels
+			_mainViewModel = new MainPageViewModel(loggedUser,this);
+			_userInfoViewModel = new UserInfoPageViewModel(loggedUser, userInfoPage);
+			_timerViewModel = new PomodoroTimerPageViewModel(loggedUser, pomodoroTimerPage);
+
+			//Setting private properties
+			this.isSliderMenuVisible = true;
+	
+		}
+
+		/// <summary>
+		/// Loads PomodoroTimerPage as application's starting page
+		/// </summary>
+		private void MainPage_OnLoaded ( object sender, RoutedEventArgs e ) {
+			
+			//Loads PomodoroTimerPage
+			this.ApplicationMainFrame.NavigationService.Navigate(userInfoPage);
+
+		}
+
+		/// <summary>
+		/// Shows / hides sliding menu
+		/// </summary>
+		private void MenuButtonClick ( object sender, RoutedEventArgs e ) {
+
+			Storyboard showMenuAnimation = this.FindResource("showDockPanel") as Storyboard;
+			Storyboard hideMenuAnimation = this.FindResource("showDockPanel") as Storyboard;
+
+			if ( ! isSliderMenuVisible ) {
+				showMenuAnimation = this.FindResource("showDockPanel") as Storyboard;
+				this.StartPageBtn.Visibility = Visibility.Visible;
+				this.PomodoroTimerBtn.Visibility = Visibility.Visible;
+				showMenuAnimation.Begin();
+				this.MoveMenuBtn.Content = "Hide";
+				isSliderMenuVisible = true;
+			}
+			else {
+				hideMenuAnimation = this.FindResource("hideDockPanel") as Storyboard;
+				hideMenuAnimation.Begin();
+				this.StartPageBtn.Visibility = Visibility.Hidden;
+				this.PomodoroTimerBtn.Visibility = Visibility.Hidden;
+				this.MoveMenuBtn.Content = "Show";
+				isSliderMenuVisible = false;
+			}
+
+		}
+
+		/// <summary>
+		/// Loads start page 
+		/// </summary>
+		private void StartPageBtn_OnClick(object sender, RoutedEventArgs e) {
+			this.ApplicationMainFrame.NavigationService.Navigate(userInfoPage);
+		}
+
+		private void PomodoroTimerBtn_OnClick(object sender, RoutedEventArgs e) {
+			this.ApplicationMainFrame.NavigationService.Navigate(pomodoroTimerPage);
+		}
+
+		
+	}
+}
